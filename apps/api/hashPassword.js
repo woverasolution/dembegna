5672a880b0bd2cbe297783fbeb31882902c495bd
tconceptt <tendalamaw@gmail.com>
2025-05-24 17:08:03 +0300

    initial auth setup vibes

---
+++ apps/api/hashPassword.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcryptjs');
+const plainPassword = 'yourSecurePassword123'; // Replace with the password you want to hash
+
+const saltRounds = 10; // Or 12 for more security
+bcrypt.genSalt(saltRounds, function(err, salt) {
+    if (err) throw err;
+    bcrypt.hash(plainPassword, salt, function(err, hash) {
+        if (err) throw err;
+        console.log('Password:', plainPassword);
+        console.log('Hashed Password:', hash);
+    });
+}); 
